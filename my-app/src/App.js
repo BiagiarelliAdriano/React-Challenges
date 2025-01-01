d0c36569b6d02f5db4c7520d59b345070640aa83
Adriano Biagiarelli <adrianobiagiarelli@gmail.com>
2025-01-01 09:49:44 +0000

    Add nesting components guide

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,13 @@
 import css from './App.module.css';
 import Sidebar from "./components/Sidebar";
 import NavBarSimple from './components/NavBarSimple';
-import EventsClass from './components/EventsClass';
-import EventsFunctional from './components/EventsFunctional';
-import EventBinding from './components/EventBinding';
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
-      <EventBinding />
+      <Sidebar />
+      <NavBarSimple />
     </div>
   );
 }
