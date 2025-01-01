c7ee0242548567b342ddadd1224e9bf945d10a49
Adriano Biagiarelli <adrianobiagiarelli@gmail.com>
2025-01-01 09:04:40 +0000

    Add binding events guide

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,15 +3,13 @@ import Sidebar from "./components/Sidebar";
 import NavBarSimple from './components/NavBarSimple';
 import EventsClass from './components/EventsClass';
 import EventsFunctional from './components/EventsFunctional';
+import EventBinding from './components/EventBinding';
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
-      <Sidebar />
-      <NavBarSimple />
-      <EventsClass />
-      <EventsFunctional />
+      <EventBinding />
     </div>
   );
 }
