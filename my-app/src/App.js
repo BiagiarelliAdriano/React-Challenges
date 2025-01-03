2724cb9cbfd6214641d8acd63b25491eaa0ecfdd
Adriano Biagiarelli <adrianobiagiarelli@gmail.com>
2025-01-03 12:47:12 +0000

    Add life cycle update method guide

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,14 +3,13 @@ import Sidebar from "./components/Sidebar";
 // import NavBarSimple from './components/NavBarSimple';
 import NavBarForm from './components/NavBarForm';
 import Content from "./components/Content";
+import LifeCyclesCDU from './components/guides/LifeCyclesCDU';
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
-      <Sidebar />
-      <NavBarForm />
-      <Content />
+      <LifeCyclesCDU />
     </div>
   );
 }
