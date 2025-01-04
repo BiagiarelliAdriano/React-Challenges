a912675be0096dfaa1cfa63c6f43e8389507dbda
Adriano Biagiarelli <adrianobiagiarelli@gmail.com>
2025-01-04 10:33:04 +0000

    Add form search bar guide

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,15 @@
 import css from './App.module.css';
-import Sidebar from "./components/Sidebar";
+// import Sidebar from "./components/Sidebar";
 // import NavBarSimple from './components/NavBarSimple';
-import NavBarForm from './components/NavBarForm';
-import Content from "./components/Content";
+// import NavBarForm from './components/NavBarForm';
+// import Content from "./components/Content";
+import SearchBar from "./components/guides/SearchBar";
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
-      <Sidebar />
-      <NavBarForm />
-      <Content />
+      <SearchBar />
     </div>
   );
 }
