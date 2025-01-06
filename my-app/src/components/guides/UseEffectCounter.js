70771c531c0dcea25ff6fcc684643bdf237a6577
Adriano Biagiarelli <adrianobiagiarelli@gmail.com>
2025-01-06 14:25:13 +0000

    Add ContentHooks.js that uses react hooks

--- my-app/src/components/guides/UseEffectCounter.js
+++ my-app/src/components/guides/UseEffectCounter.js
@@ -2,9 +2,15 @@ import React, { useState, useEffect } from 'react'
 
 function UseEffectCounter() {
     const [count, setCount] = useState(0);
+    const [time, setTime] = useState(0);
     useEffect(() => {
         document.title = count;
-    });
+    }, [count]);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(time => time + 1)
+        }, 1000);
+    }, []);
     return (
         <div>
             <button onClick={
@@ -12,6 +18,7 @@ function UseEffectCounter() {
             }>
                 Increment Count ({count})
             </button>
+            <h1>Time is {time}</h1>
         </div>
     )
 }
